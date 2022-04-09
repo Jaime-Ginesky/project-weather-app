bfe7ee6a61f94789be056e784a5d01132cc45e67
Jaime G <jaimelee79@gmail.com>
2022-04-09 14:49:36 -0700

    Added GitHub url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,22 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a href="https://naughty-hermann-a4b01e.netlify.app/" target="_blank">
+          Jaime Ginesky{" "}
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Jaime-Ginesky/project-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub.
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-App;
