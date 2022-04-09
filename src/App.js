397a00e87ce29b320f2098d532e7c30358473a1d
Jaime G <jaimelee79@gmail.com>
2022-04-09 15:17:06 -0700

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,20 +5,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a href="https://naughty-hermann-a4b01e.netlify.app/" target="_blank">
-          Jaime Ginesky{" "}
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Jaime-Ginesky/project-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub.
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://naughty-hermann-a4b01e.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Jaime Ginesky{" "}
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Jaime-Ginesky/project-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
