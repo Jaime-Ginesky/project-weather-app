1f394bb02af506a68e66cdbd7e49160edefe7d48
Jaime G <jaimelee79@gmail.com>
2022-04-09 17:27:19 -0700

    added framework of app in weather component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         Weather App
+        <Weather />
         <footer>
           This project was coded by{" "}
           <a
